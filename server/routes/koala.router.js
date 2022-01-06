85d147a480179b6628c59c8d3e6f2451ec9c6769
Sabrina Ciaciura <ciaci0028@gmail.com>
2022-01-06 14:57:18 -0600

    added order by name

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -21,7 +21,7 @@ pool.on('error', (err) => {
 
 // GET
 koalaRouter.get('/', (req,res) => {
-      let queryText = `SELECT * FROM "koalas"`
+      let queryText = `SELECT * FROM "koalas" ORDER BY "name"`
       pool.query(queryText).then((result) => {
             res.send(result.rows);
       }).catch((err) => {
